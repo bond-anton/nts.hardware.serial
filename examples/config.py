762719f37739a24ab6b770f761ce481795e16c7e
bond_anton <bond.anton@gmail.com>
2025-02-26 14:33:57 +0300

    module docstring

--- examples/config.py
+++ examples/config.py
@@ -1,8 +1,11 @@
+"""Example of SerialConnectionConfig class usage."""
+
 from nts.hardware.serial.config.serial_connection_implementation import (
     SerialConnectionMinimalConfig,
     SerialConnectionConfig,
     ModbusSerialConnectionConfig,
 )
+
 
 min_conf = SerialConnectionMinimalConfig(port="COM1", bytesize=7, parity="N")
 ser_conf = SerialConnectionConfig(port="COM1", bytesize=7, write_timeout=1.0)
