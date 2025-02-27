43c0da4e6f5a8d1338c67982ec708face0e0fcdd
bond_anton <bond.anton@gmail.com>
2025-02-27 10:24:50 +0300

    VSN examples

---
+++ examples/virtual_serial_pair.py
@@ -0,0 +1,9 @@
+from nts.hardware.serial import VirtualSerialPair
+
+if __name__ == "__main__":
+    vsp = VirtualSerialPair()
+    vsp.start()
+    # Now your virtual serial pair is ready
+    print(vsp.serial_ports)
+
+    vsp.stop()
